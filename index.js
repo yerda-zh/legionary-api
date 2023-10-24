d86af2d91c64d773522acd687e430afda9cbeda5
yerda-zh <yerdaulet.04@gmail.com>
2023-10-25 01:16:21 +0600

    added delete endpoint, improved comprehensiveness

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-const express = require("express");
-const OpenAI = require("openai");
-const cors = require("cors");
+import express from "express";
+import OpenAI from "openai";
+import cors from "cors";
+import bcrypt from "bcrypt-nodejs";// used for hashing passwords
+import knex from "knex";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
-require("dotenv").config();
-
-const bcrypt = require("bcrypt-nodejs");
-
-const knex = require("knex")({
-  client: "pg",
-  connection: {
-    host: "127.0.0.1",
-    port: 5432,
-    user: "postgres",
-    password: "yerda.Sql",
-    database: "legionary_database",
-  },
-});
 
 app.use(express.json());
 app.use(cors());
@@ -27,13 +19,27 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+// configuring knex.js which helps to write manage postgresql
+const dbConfig = {
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    port: 5432,
+    user: 'postgres',
+    password: 'yerda.Sql',
+    database: 'legionary_database',
+  },
+};
+
+const database = knex(dbConfig);
+
 app.post("/chat", async (req, res) => {
   try {
     const { answers } = req.body;
-
     if (answers.length === 0) {
       return res.status(400).json("No data was provided.");
     }
+
     // Create prompt text with user input
     const prompt = `Create personalized workout routine with sets and reps for a ${answers[0]}, within age category of ${answers[1]}, goal - ${answers[2]}, body type - ${answers[3]}, level of body fat - ${answers[5]}, level of fitness - ${answers[6]}, place for workouts - ${answers[7]}, willing to spend - ${answers[8]}.`;
     //Define the JSON Schema by creating a schema object
@@ -46,7 +52,7 @@ app.post("/chat", async (req, res) => {
         },
         equipment: {
           type: "array",
-          description: "needed equipment for workout routine",
+          description: "Needed equipment for workout routine",
           items: { type: "string" },
         },
         routine: {
@@ -97,88 +103,61 @@ app.post("/chat", async (req, res) => {
       },
     };
 
-    const response = await openai.chat.completions
-      .create({
-        model: "gpt-3.5-turbo-0613",
-        messages: [
-          {
-            role: "system",
-            content: "You are a workout routine creating trainer",
-          },
-          { role: "user", content: prompt },
-        ],
-        functions: [{ name: "workout_routine", parameters: schema }],
-        function_call: { name: "workout_routine" },
-      })
-      .then((res) =>
-        JSON.parse(res.choices[0].message.function_call.arguments)
-      );
-
-    return res.status(200).json({
-      data: response,
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo-0613",
+      messages: [
+        {
+          role: "system",
+          content: "You are a workout routine creating trainer",
+        },
+        { role: "user", content: prompt },
+      ],
+      functions: [{ name: "workout_routine", parameters: schema }],
+      function_call: { name: "workout_routine" },
     });
+
+    const responseData = JSON.parse(response.choices[0].message.function_call.arguments);
+
+    return res.status(200).json({ data: responseData });
   } catch (error) {
     return res.status(400).json({
-      error: error.response
-        ? error.response.data
-        : "There was an issue on the server",
+      error: error.response ? error.response.data : "There was an issue on the server",
     });
   }
 });
 
-app.post("/test", async (req, res) => {
-  try {
-    const { answers } = req.body;
-    const response = answers;
-
-    return res.status(200).json({
-      success: true,
-      data: response,
-    });
-  } catch (error) {
-    return res.status(400).json({
-      success: false,
-      error: error.response
-        ? error.response.data
-        : "There was an issue on the server",
-    });
-  }
-});
-
-app.get("/profile/:id", (req, res) => {
+app.get("/profile/:id", async (req, res) => {
   const { id } = req.params;
-  knex
-    .select("*")
-    .from("users")
-    .where({ id })
-    .then((user) => {
-      if (user.length) {
-        res.json(user[0]);
-      } else {
-        res.status(400).json("Not Found");
-      }
-    })
-    .catch((err) => res.status(400).json("Error getting user"));
+
+  try{
+    const user = await database.select("*").from("users").where({ id });
+
+    if (user.length) {
+      res.json(user[0]);
+    } else {
+      res.status(400).json("Not Found");
+    }
+  } catch (error) {
+    res.status(400).json("Error getting user");
+  }
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   const { email, name, password } = req.body;
 
+  //checks whether front end provided data or not
   if (!email || !password || !name) {
     return res.status(400).json("Incorrect form submission");
-  } //checks whether front end provided data or not
+  }
 
-  const hash = bcrypt.hashSync(password); // to convert password into hash so that we can store them in database
-
+  // to convert password into hash so that we can store them in database
+  const hash = bcrypt.hashSync(password);
+  
   // transaction allows to make multiple database operations
   // firstly we store email and password to login then store email, name, joined to users, then store user id into workout table
-  knex
+  database
     .transaction((trx) => {
-      trx
-        .insert({
-          hash: hash,
-          email: email,
-        })
+      trx.insert({hash, email})
         .into("login")
         .returning("email")
         .then((loginEmail) => {
@@ -200,84 +179,98 @@ app.post("/register", (req, res) => {
         .then(trx.commit)
         .catch(trx.rollback);
     })
-    .catch((err) => res.status(400).json("unable to register"));
+    .catch((err) => res.status(400).json("Unable to register"));
 });
 
-app.post("/signin", (req, res) => {
+app.post("/signin", async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.status(400).json("incorrect form submission");
+    return res.status(400).json("Incorrect form submission");
   }
-  knex
-    .select("email", "hash")
-    .from("login")
-    .where("email", "=", email)
-    .then((data) => {
-      const isValid = bcrypt.compareSync(password, data[0].hash);
-      if (isValid) {
-        return knex
-          .select(
-            "users.id",
-            "users.name",
-            "users.email",
-            "users.bmi",
-            "users.joined",
-            "workout.routine"
-          )
-          .from("users")
-          .join("workout", "users.id", "workout.user_id")
-          .select("workout.routine")
-          .where("email", "=", email)
-          .then((user) => res.json(user[0]))
-          .catch((err) => res.status(400).json("unable to get user"));
-      } else {
-        res.status(400).json("wrong credentials");
-      }
-    })
-    .catch((err) => res.status(400).json("wrong credentials"));
+
+  try {
+    const data = await database.select("email", "hash").from("login").where("email", "=", email);
+    const isValid = bcrypt.compareSync(password, data[0].hash);
+
+    if (isValid) {
+      const user = await database
+        .select(
+          "users.id",
+          "users.name",
+          "users.email",
+          "users.bmi",
+          "users.joined",
+          "workout.routine"
+        )
+        .from("users")
+        .join("workout", "users.id", "workout.user_id")
+        .select("workout.routine")
+        .where("email", "=", email);
+
+      res.json(user[0]);
+    } else {
+      res.status(400).json("Wrong credentials");
+    }
+  } catch (error) {
+    res.status(400).json("Wrong credentials");
+  }
 });
 
 app.put("/save", (req, res) => {
   const { id, bmi, routine } = req.body;
 
   if (!id || !bmi || !routine) {
-    return res.status(400).json("unable to save routine");
+    return res.status(400).json("Unable to save routine");
   }
 
   // saves the workout routine as json into workout table where user id matches
-  knex
+  database
     .transaction((trx) => {
       trx("workout")
         .where({ user_id: id })
-        .update({
-          routine: routine,
-        })
+        .update({routine})
         .then((numUpdatedRows) => {
           if (numUpdatedRows > 0) {
-            res.status(200).json({ message: "success" });
+            res.status(200).json("Success");
           } else {
             res.status(400).json("No such user");
           }
-          return trx("users").where({ id: id }).update({
-            bmi: bmi,
-          });
+
+          return trx("users").where({ id: id }).update({bmi});
         })
         .then(trx.commit)
         .catch(trx.rollback);
     })
-    .catch((err) => res.status(400).json(err));
-  // knex("workout")
-  //   .where({ user_id: id })
-  //   .update({
-  //     routine: routine,
-  //   })
-  //   .then((numUpdatedRows) => {
-  //     if (numUpdatedRows > 0) {
-  //       res.status(200).json({ message: "success" });
-  //     } else {
-  //       res.status(400).json("No such user");
-  //     }
-  //   })
-  //   .catch((err) => res.status(400).json(err));
+    .catch((error) => res.status(400).json("Unable to save the data"));
 });
+
+app.delete("/delete", (req, res) => {
+  const { id } = req.body;
+
+  if (!id) {
+    return res.status(400).json("Unable to delete the user");
+  }
+
+  database.transaction((trx) => {
+    trx("workout").where({ user_id: id }).del()
+      .then(() => {
+        return trx("login").where({ id }).del();
+      })
+      .then(() => {
+        return trx("users").where({ id }).del();
+      })
+      .then(trx.commit)
+      .catch((error) => {
+        trx.rollback();
+        res.status(400).json("Unable to delete");
+      });
+  })
+  .then(() => {
+    res.json("Success");
+  })
+  .catch((err) => {
+    res.status(400).json("Unable to delete");
+  });
+});
+
